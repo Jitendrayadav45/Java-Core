1b382d29d62e81f3e1bd70de3702074d63b87d60
Jitendra Yadav <official.jitendraa@gmail.com>
2024-11-28 21:46:20 +0530

    Initial commit: Add ShiftOperators.java

--- ShiftOperators.java
+++ ShiftOperators.java
@@ -2,12 +2,23 @@ import java.util.Scanner;
 
 public class ShiftOperators {
     public static void main(String[] args) {
+        // Create a Scanner object to read user input
         Scanner sc = new Scanner(System.in);
+        
+        // Prompt the user to enter a number
         System.out.print("Enter a number: ");
-        int a = sc.nextInt();
+        int a = sc.nextInt(); // Read the integer input from the user
 
+        // Perform left shift operation and display the result
         System.out.println("Left shift (a << 2): " + (a << 2));
+        
+        // Perform right shift operation and display the result
         System.out.println("Right shift (a >> 2): " + (a >> 2));
+        
+        // Perform unsigned right shift operation and display the result
         System.out.println("Unsigned right shift (a >>> 2): " + (a >>> 2));
+        
+        // Close the scanner
+        sc.close();
     }
 }
