2e3f553e7e2e4bd66fd0d1f321393633829d665c
Jitendra Yadav <official.jitendraa@gmail.com>
2024-12-10 22:25:37 +0530

    java pattern matching question:

---
+++ RotateMatrix90.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class RotateMatrix90 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input size of square matrix
+        System.out.print("Enter the size of the square matrix: ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+        System.out.println("Enter the matrix elements:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Rotate the matrix in-place
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                // Swap elements
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        // Print rotated matrix
+        System.out.println("Rotated Matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
